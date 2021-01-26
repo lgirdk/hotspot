b298550f863f5206c40b91860da706697d5bdc20
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-01-26 12:21:48 +0100

    Support for healthcheck and failover mechanism ( RDKB-5046 )
    Modifying health check fail over mechanism according to requirement (MVXREQ-1237).
    
    1.If only single end point configured or health check disabled (X_LGI-COM_HealthCheckInterval is zero) should consider primary EP as active and create tunnel.
    2.If both endpoints are down should wait for X_LGI-COM_HealthCheckInterval before next availabilty check.
    3.Added a flag to keep track of number of endpoints configured / If number of endpoints is greater than zero then only Hotspot component will start.
    
    TESTING PERFORMED:-
    
    1.Make DUT up after factory reset.
    2.Verified that the sysevent parameters hold default value from PSM.
    3.Verified if X_LGI-COM_HealthCheckInterval is zero/ Only signle EP configured the primary EP considerd as active without availability check.
    4.Verified if X_LGI-COM_HealthCheckInterval is greater than 0 and X_LGI-COM_RecoveryCheckInterval is undefined,
        the Gateway MUST default RecoveryCheckInterval to 10x the HealthCheckInterval.
    5.Verified if Primary EP is available it is considerd as active and availablity check is done in every X_LGI-COM_HealthCheckInterval seconds.
    6.Verified if primary EP is not available switched to secondary EP and continued there until secondary becomes down/ GRE configuration changes.
    7.Verified if both EP's are not available , The next check for availablity of Primary EP is done after X_LGI-COM_RecoveryCheckInterval.

--- source/hotspotfd/include/hotspotfd.h
+++ source/hotspotfd/include/hotspotfd.h
@@ -55,6 +55,7 @@
 
 #define kHotspotfd_primary                  "hotspotfd-primary"                // ip address of primary
 #define khotspotfd_secondary                "hotspotfd-secondary"              // ip address of secondary
+#define khotspotfd_ep_count                 "hotspotfd-ep-count"               // Tracks the number of EP's configured
 #define khotspotfd_keep_alive               "hotspotfd-keep-alive"             // time in secs between pings
 #define khotspotfd_keep_alive_threshold     "hotspotfd-threshold"              // failed ping's before switching EP
 #define khotspotfd_max_secondary            "hotspotfd-max-secondary"          // max. time allowed on secondary
@@ -105,6 +106,7 @@ typedef struct
 typedef enum {
     HOTSPOTFD_PRIMARY,
     HOTSPOTFD_SECONDARY,
+    HOTSPOTFD_EPCOUNT,
     HOTSPOTFD_KEEPALIVE,
     HOTSPOTFD_THRESHOLD,
     HOTSPOTFD_MAXSECONDARY,
