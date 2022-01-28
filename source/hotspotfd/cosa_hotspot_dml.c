d24a4d72d9d10fa940431cb87e30e02cf72a2d85
csaisu662 <Chenchireddygari_SaiSujithKumarReddy@comcast.com>
2022-01-28 04:21:30 +0000

    RDKB-38633 : CcspHotSpot - Stack Buffer Overflow
    Reason for change: Validating MAC address
    Test Procedure: Build and verify Device.X_COMCAST-COM_GRE.Hotspot.ClientChange
    Risks: None
    
    Signed-off-by: csaisu662 <Chenchireddygari_SaiSujithKumarReddy@comcast.com>
    Change-Id: Ia438e5c39b2089709bcb1b8ef367b97e6d0dedcd

--- source/hotspotfd/cosa_hotspot_dml.c
+++ source/hotspotfd/cosa_hotspot_dml.c
@@ -72,49 +72,86 @@
 
 #include <telemetry_busmessage_sender.h>
 #include "safec_lib_common.h"
+#include <ctype.h>
+#define MAC_ADDRESS_BYTES 18
+#define MAC_ADDRESS_CHECK_INCR 3
+
+/* Validates the colon separated hexadecimal format xx:xx:xx:xx:xx:xx */
+static int isValidMAC(char *macAddress)
+{
+    int i;
+    for (i = 0; i < MAC_ADDRESS_BYTES/MAC_ADDRESS_CHECK_INCR; i++)
+    {
+        if (!(isxdigit(macAddress[0])) || (!isxdigit(macAddress[1])) || !(macAddress[2] == ((i ==(MAC_ADDRESS_BYTES/MAC_ADDRESS_CHECK_INCR)-1) ? '\0' : ':')))
+                return -1;
+        macAddress+=MAC_ADDRESS_CHECK_INCR;
+    }
+    return 0;
+}
+
 
 BOOL HotspotConnectedDevice_SetParamStringValue(ANSC_HANDLE hInsContext, char* ParamName, char* strValue)
 {
     UNREFERENCED_PARAMETER(hInsContext);
-    int l_iAddOrDelete, l_iSsidIndex, l_iRssi; 
-    char l_cMacAddr[20];
+    int l_iAddOrDelete, l_iSsidIndex, l_iRssi;
+    char l_cMacAddr[17];
     errno_t rc = -1;
     int ind = -1;
     char *ret = NULL;
+    int count =0;
 
+    if(strValue == NULL){
+       CcspTraceError(("Received null strValue\n"));
+       return FALSE;
+    }
     rc = memset_s(l_cMacAddr, sizeof(l_cMacAddr), '\0', sizeof(l_cMacAddr));
     ERR_CHK(rc);
 
     rc = strcmp_s("ClientChange", strlen("ClientChange"),ParamName, &ind);
     ERR_CHK(rc);
-    if ((ind == 0) && (rc == EOK))
-    {   
-                ret = strrchr(strValue, '|');
-                if((ret != NULL) && (strlen(++ret) >= sizeof(l_cMacAddr)))
-                    return FALSE;
-                CcspTraceInfo(("Received Client MAC and other details: %s\n", strValue));
-                rc = sscanf_s(strValue, "%d|%d|%d|%s", &l_iAddOrDelete, &l_iSsidIndex, &l_iRssi, l_cMacAddr);
-                if(rc < EOK || rc == EOF)
-                {
-                    ERR_CHK(rc);
-                    return FALSE;
-                }
-
-		if (1 == l_iAddOrDelete)
-		{
-		        CcspTraceInfo(("Added case, Client with MAC:%s will be added\n", l_cMacAddr));
-		        t2_event_d("WIFI_INFO_Hotspot_client_connected", 1);
-			updateRssiForClient(l_cMacAddr, l_iRssi);
-		}
-		else
-		{
-		        CcspTraceInfo(("Removal case, Client with MAC:%s will be removed \n", l_cMacAddr));
-		        t2_event_d("WIFI_INFO_Hotspot_client_disconnected", 1);
-			snoop_RemoveClientListEntry(l_cMacAddr);
-		}	
-        return TRUE;
+    if (ind != 0 || rc != EOK) {
+        return FALSE;
     }
-    return FALSE;
+   for (ret = strValue; *ret ; ++ret) {
+    if (*ret == '|' && ++count == 3) {
+            ++ret;    // ret points to first char after 3rd "|"
+            break;
+        }
+    }
+    if (count != 3) {
+        return FALSE;
+    }
+    if (strlen(ret) > sizeof(l_cMacAddr)) {
+        CcspTraceError(("Invalid Client MAC address length %d, expected %d\n", strlen(ret), sizeof(l_cMacAddr)));
+        return FALSE;
+    }
+    if(isValidMAC(ret) != 0)
+    {
+            CcspTraceError(("Invalid Client MAC address Format\n"));
+            return FALSE;
+    }
+
+    CcspTraceInfo(("Received Client MAC and other details: %s\n", strValue));
+    rc = sscanf_s(strValue, "%d|%d|%d|%s", &l_iAddOrDelete, &l_iSsidIndex, &l_iRssi, l_cMacAddr);
+    if(rc < EOK || rc == EOF || rc != 4)     // 3rd condition, so sscanf_s must find exactly 4 values
+    {
+        ERR_CHK(rc);
+        return FALSE;
+    }
+
+	if (1 == l_iAddOrDelete)
+	{
+	        CcspTraceInfo(("Added case, Client with MAC:%s will be added\n", l_cMacAddr));
+	        t2_event_d("WIFI_INFO_Hotspot_client_connected", 1);
+		updateRssiForClient(l_cMacAddr, l_iRssi);
+	}
+	else
+	{
+	        CcspTraceInfo(("Removal case, Client with MAC:%s will be removed \n", l_cMacAddr));
+	        t2_event_d("WIFI_INFO_Hotspot_client_disconnected", 1);
+		snoop_RemoveClientListEntry(l_cMacAddr);
+	}
+    return TRUE;
 }
 
 ULONG
