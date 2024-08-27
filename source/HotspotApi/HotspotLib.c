a871aeb9c4eefdf0079b90abdf363ebe4615c504
Andre McCurdy <armccurdy@gmail.com>
2024-08-27 15:29:09 -0700

    misc code cleanup
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/HotspotApi/HotspotLib.c
+++ source/HotspotApi/HotspotLib.c
@@ -27,66 +27,70 @@
 /**************************************************************************/
 /*      GLOBAL and STATIC  VARIABLES                                      */
 /**************************************************************************/
+
 /* Array for mapping vlan and brdige interface */
+
+vlanSyncData_s gVlanSyncData[] = {
 #if (defined(_XB6_PRODUCT_REQ_) && !defined(_XB7_PRODUCT_REQ_))
-vlanSyncData_s gVlanSyncData[] = {
-     {VAP_NAME_4, "ath4", "brlan2", 0x1, 5, 1},
-     {VAP_NAME_5, "ath5", "brlan3", 0x2, 6, 2},
-     {VAP_NAME_8, "ath8", "brlan4", 0x4, 9, 3},
-     {VAP_NAME_9, "ath9", "brlan5", 0x8, 10, 4}
-};
+
+    {VAP_NAME_4, "ath4", "brlan2", 0x1, 5, 1},
+    {VAP_NAME_5, "ath5", "brlan3", 0x2, 6, 2},
+    {VAP_NAME_8, "ath8", "brlan4", 0x4, 9, 3},
+    {VAP_NAME_9, "ath9", "brlan5", 0x8, 10, 4}
+
 #elif defined(_XB8_PRODUCT_REQ_) && defined(RDK_ONEWIFI)
-vlanSyncData_s gVlanSyncData[] = {
-     {VAP_NAME_4, "wl0.3", "brlan2", 0x1, 5, 1},
-     {VAP_NAME_5, "wl1.3", "brlan3", 0x2, 6, 2},
-     {VAP_NAME_8, "wl0.5", "brlan4", 0x4, 9, 3},
-     {VAP_NAME_9, "wl1.5", "brlan5", 0x8, 10, 4},
-     {VAP_NAME_11, "wl2.3", "bropen6g", 0x16, 19, 46},
-     {VAP_NAME_12, "wl2.5", "brsecure6g", 0x32, 21, 47}
-};
+
+    {VAP_NAME_4, "wl0.3", "brlan2", 0x1, 5, 1},
+    {VAP_NAME_5, "wl1.3", "brlan3", 0x2, 6, 2},
+    {VAP_NAME_8, "wl0.5", "brlan4", 0x4, 9, 3},
+    {VAP_NAME_9, "wl1.5", "brlan5", 0x8, 10, 4},
+    {VAP_NAME_11, "wl2.3", "bropen6g", 0x16, 19, 46},
+    {VAP_NAME_12, "wl2.5", "brsecure6g", 0x32, 21, 47}
+
 #elif defined(_XB7_PRODUCT_REQ_) && defined(RDK_ONEWIFI)
-vlanSyncData_s gVlanSyncData[] = {
+
 #if defined(_INTEL_WAV_)
-     {VAP_NAME_4, "wlan0.2", "brlan2", 0x1, 5, 1},
-     {VAP_NAME_5, "wlan2.2", "brlan3", 0x2, 6, 2},
-     {VAP_NAME_8, "wlan0.4", "brlan4", 0x4, 9, 3},
-     {VAP_NAME_9, "wlan2.4", "brlan5", 0x8, 10, 4}
+    {VAP_NAME_4, "wlan0.2", "brlan2", 0x1, 5, 1},
+    {VAP_NAME_5, "wlan2.2", "brlan3", 0x2, 6, 2},
+    {VAP_NAME_8, "wlan0.4", "brlan4", 0x4, 9, 3},
+    {VAP_NAME_9, "wlan2.4", "brlan5", 0x8, 10, 4}
 #else
-     {VAP_NAME_4, "wl0.3", "brlan2", 0x1, 5, 1},
-     {VAP_NAME_5, "wl1.3", "brlan3", 0x2, 6, 2},
-     {VAP_NAME_8, "wl0.5", "brlan4", 0x4, 9, 3},
-     {VAP_NAME_9, "wl1.5", "brlan5", 0x8, 10, 4}
+    {VAP_NAME_4, "wl0.3", "brlan2", 0x1, 5, 1},
+    {VAP_NAME_5, "wl1.3", "brlan3", 0x2, 6, 2},
+    {VAP_NAME_8, "wl0.5", "brlan4", 0x4, 9, 3},
+    {VAP_NAME_9, "wl1.5", "brlan5", 0x8, 10, 4}
 #endif
-};
-#elif defined (_XB7_PRODUCT_REQ_)|| defined (_XF3_PRODUCT_REQ_)
-vlanSyncData_s gVlanSyncData[] = {
+
+#elif defined (_XB7_PRODUCT_REQ_) || defined(_XF3_PRODUCT_REQ_)
+
 #if defined(_INTEL_WAV_)
-     {VAP_NAME_4, "wlan0.2", "brlan2", 0x1, 5, 1},
-     {VAP_NAME_5, "wlan2.2", "brlan3", 0x2, 6, 2},
-     {VAP_NAME_8, "wlan0.4", "brlan4", 0x4, 9, 3},
-     {VAP_NAME_9, "wlan2.4", "brlan5", 0x8, 10, 4}
+    {VAP_NAME_4, "wlan0.2", "brlan2", 0x1, 5, 1},
+    {VAP_NAME_5, "wlan2.2", "brlan3", 0x2, 6, 2},
+    {VAP_NAME_8, "wlan0.4", "brlan4", 0x4, 9, 3},
+    {VAP_NAME_9, "wlan2.4", "brlan5", 0x8, 10, 4}
 #else
-     {VAP_NAME_4, "wl0.2", "brlan2", 0x1, 5, 1},
-     {VAP_NAME_5, "wl1.2", "brlan3", 0x2, 6, 2},
-     {VAP_NAME_8, "wl0.4", "brlan4", 0x4, 9, 3},
-     {VAP_NAME_9, "wl1.4", "brlan5", 0x8, 10, 4}
+    {VAP_NAME_4, "wl0.2", "brlan2", 0x1, 5, 1},
+    {VAP_NAME_5, "wl1.2", "brlan3", 0x2, 6, 2},
+    {VAP_NAME_8, "wl0.4", "brlan4", 0x4, 9, 3},
+    {VAP_NAME_9, "wl1.4", "brlan5", 0x8, 10, 4}
+#endif
+
+#elif defined(_CBR_PRODUCT_REQ_)
+
+    {VAP_NAME_4, "wl0.2", "brlan2", 0x1, 5, 1},
+    {VAP_NAME_5, "wl1.2", "brlan3", 0x2, 6, 2},
+    {VAP_NAME_8, "wl0.4", "brlan4", 0x4, 9, 3},
+    {VAP_NAME_9, "wl1.4", "brlan5", 0x8, 10, 4},
+    {VAP_NAME_10, "wl1.7", "brpublic", 0x16, 16, 45}
+
+#else
+
+    {VAP_NAME_4, "NULL", "NULL", 0x1, 0, 1},
+    {VAP_NAME_5, "NULL", "NULL", 0x2, 0, 2},
+    {VAP_NAME_8, "NULL", "NULL", 0x4, 0, 3},
+    {VAP_NAME_9, "NULL", "NULL", 0x8, 0, 4}
+
 #endif
 };
-#elif defined (_CBR_PRODUCT_REQ_)
-vlanSyncData_s gVlanSyncData[] = {
-     {VAP_NAME_4, "wl0.2", "brlan2", 0x1, 5, 1},
-     {VAP_NAME_5, "wl1.2", "brlan3", 0x2, 6, 2},
-     {VAP_NAME_8, "wl0.4", "brlan4", 0x4, 9, 3},
-     {VAP_NAME_9, "wl1.4", "brlan5", 0x8, 10, 4},
-     {VAP_NAME_10, "wl1.7", "brpublic", 0x16, 16, 45}
-};
-#else
-vlanSyncData_s gVlanSyncData[] = {
-     {VAP_NAME_4, "NULL", "NULL", 0x1, 0, 1},
-     {VAP_NAME_5, "NULL", "NULL", 0x2, 0, 2},
-     {VAP_NAME_8, "NULL", "NULL", 0x4, 0, 3},
-     {VAP_NAME_9, "NULL", "NULL", 0x8, 0, 4}
-};
-#endif
 
 int gVlanSyncDataSize = ARRAY_SIZE(gVlanSyncData);
